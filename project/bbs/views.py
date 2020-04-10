adbf4c11fcde2faad0b976d39e6c535f33114a7a
lzh-sjtu <1084388051@qq.com>
2020-04-10 23:04:47 +0800

    index add paginator

--- project/bbs/views.py
+++ project/bbs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
 from . import models, forms
 from login.models import User
 
@@ -7,9 +8,21 @@ from login.models import User
 def index(request):
     if not request.session.get('is_login', None):
         return redirect("/login/login/")
-    posts = models.Post.objects.all()
-    ctx = {'posts': posts}
-    return render(request,'bbs/index.html',ctx)
+    posts_list = models.Post.objects.order_by('-create_time')
+    paginator = Paginator(posts_list, 3)
+    if request.method == "GET":
+        page = request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except InvalidPage:
+            return HttpResponse('找不到页面的内容')
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+    else:
+         posts = paginator.page(1)
+    return render(request, 'bbs/index.html', {'posts': posts, "length": len(posts_list)})
 
 
 def bbs_detail(request, post_id):
@@ -37,11 +50,7 @@ def post_edit_page_action(request):
             content = post_form.cleaned_data.get('content')
             category = post_form.cleaned_data.get('category')
             tag = post_form.cleaned_data.get('tag')
-
-    author = User.objects.get(sno=request.session['user_sno'])
-
-    post = models.Post.objects.create(title=title, content=content, author=author, category=category)
-    post.tags.add(tag)
-
-    posts = models.Post.objects.all()
-    return render(request, 'bbs/index.html', {'posts': posts})
+            author = User.objects.get(sno=request.session['user_sno'])
+            post = models.Post.objects.create(title=title, content=content, author=author, category=category)
+            post.tags.add(tag)
+    return index(request)
